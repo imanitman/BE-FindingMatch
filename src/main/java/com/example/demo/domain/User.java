de90292ab96d8ed66ca9e21c80fc6306d72dd1cc
unknown <amn1022004@gmail.com>
2024-08-08 11:38:43 +0700

    init

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name= "users")
+@Getter
+@Setter
+public class User {
+    
+    
+
+}
